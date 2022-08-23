6c64c3d00b54bc6a13d8796efd0ed7ffbea0377e
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-08-23 15:05:06 +0530

    add CastingDemo.java

--- src/com/demo/CastingDemo.java
+++ src/com/demo/CastingDemo.java
@@ -6,8 +6,16 @@ public class CastingDemo {
         // o.display();
         CastingDemo castingDemo = (CastingDemo) o;
         castingDemo.display();
+        String name = "Ramesh";
+        castingDemo = (CastingDemo) ((Object)name);
+        System.out.println(o);
     }
     public void display(){
         System.out.println("demo");
+    }
+
+    @Override
+    public String toString() {
+        return "CastingDemo{}";
     }
 }
